b0975a4f432c9df1630393110a77de0f3dfb945c
Davi Viana Silva <67454522+DaviViana-p@users.noreply.github.com>
2025-05-23 17:20:00 -0300

    Update sendEmail.js

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -6,43 +6,37 @@ export default async function handler(req, res) {
   const token = tokenPart.split(":")[1]; 
   const messageWithoutToken = messageBody.replace(tokenPart, "").trim();  
   const cleanedMessage = messageWithoutToken.replace(/^,|,$/g, '').trim();
+
+  const recaptchaResponse = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+    { method: 'POST' }
+  );
+
+  const data = await recaptchaResponse.json();
+
+  if (!data.success) {
+    return res.status(400).json({ message: "ReCAPTCHA failed" });
+  }
+
   const client = new SMTPClient({
     user: process.env.EMAIL,
     password: process.env.PASSWORD,
-    host: "smtp.gmail.com",
+    host: "smtp.zoho.com",
     ssl: true,
+    port: 465,
   });
-  //console.log(token);
 
-  fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`, {
-    method: 'POST',
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      if (data.success) {
-        console.log("Success");
-        try {
-          const message =  client.sendAsync({
-            text: cleanedMessage,
-            from: "rodrigo@rvstopografia.com",
-            to: "rodrigo@rvstopografia.com",
-            subject: "Lead - Landingpage",
-          });
-          //console.log(message);
-        } catch (err) {
-          console.error(err);
-        }
-        
-      }
-      else {
-        console.log("Fail");
-      }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
+  try {
+    await client.sendAsync({
+      text: cleanedMessage,
+      from: "rodrigo@rvstopografia.com",
+      to: "contato@rvstopografia.com",
+      subject: "Lead - Landingpage",
     });
-  
 
-  res.status(200).json({ message: "Send Mail" });
+    res.status(200).json({ message: "Email enviado com sucesso" });
+  } catch (err) {
+    console.error("Erro ao enviar e-mail:", err);
+    res.status(500).json({ message: "Erro ao enviar e-mail" });
+  }
 }
